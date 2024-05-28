38a7472ee8d02caf88eec96504645348d2f8c27c
yuliana2406 <yyvelasquezg@ufpso.edu.co>
2024-05-28 12:08:06 -0500

    load file

--- JS/functions.js
+++ JS/functions.js
@@ -36,10 +36,11 @@ console.log("Mi edad es: " + edad );
 
 function load_page() {
     Swal.fire({
-        imageUrl: "https://ufpso.edu.co/administradoru15/ventana/banner%20Admitidos_Mesa%20de%20trabajo%201%20(3)%20(1).jpg",
-        showConfirmButton: false,
+        imageUrl: "https://ufpso.edu.co/administradoru15/ventana/banner%20Admitidos_Mesa%20de%20trabajo%201%20(3)%20(1).jpg",        showConfirmButton: false,
+        // imageHeigth: 1500,
+        ImageAlt: "A tall image",
         timer: 2000
-      });
+    });
 
       document.getElementById("text").innerText="el array calcular es:" + array_num;
       document.getElementById("impresion").value = array_num;
@@ -56,13 +57,14 @@ function load_page() {
       console.log(hour_system.getMonth());
       console.log(hour_system.getSeconds());
       console.log(hour_system.getTime());
-
+      let month=["enero", "febrero", "marzo ", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
       let dias = ["domingo","Lunes", "martes", "miercoles", "jueves", "viernes", "sabado"];
-      for(let i=0;i<dias.length,i++){
-        console.log(dias[dias]);
+      console.log (dias);
+      console.log (month);
+      for(let i=0; i < dias.length, i++){
+        console.log(dias [dias]);
       }
 
-      console.log("hoy es:  " + dias[dia]);
       let dia_mes = hour_system.getMonth() + 1;
       console.log(hour_system.getDate()+"/"+dia_mes+"/"+hour_system.getfullYear());
 
@@ -76,9 +78,9 @@ function send_form(){
     let pass_two   = document.getElementById("pass_two").value;
     if(name.length == 0 || last_name.length == 0|| pass_one.length == 0 || pass_two .length == 0){
         Swal.fire({
+            icon: "error",
             title: "Cajas de texto vacias",
             text: "Alguna de las cajas de texto esta vacia",
-            icon: "error"
         });
     //         if( name.length == 0){
     //         document.getElementById("name").style.border = "2px solid red"
@@ -95,21 +97,21 @@ function send_form(){
     // }
     } else if (pass_one != pass_two){
        Swal.fire({
+           icon: "error"
            title: "sus contraseñas no son iguales",
            text: "por favor valide sus credenciales",
-           icon: "error"
         });
     }
     else{
-       document.getElementById("print").innerText ="su nombre es: " +name+" "+ last_name;
+       document.getElementById("print").innerText ="su nombre es "+ name+ " "+ "y su apellido es " +last_name;
       
     }
-}
+};
 
 //ARRAYS
 var array_semana = ["Lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"];
 var array_gral = [1,2,2,5,"abc","soy Yuliana"];
-var array_num = [1,2,3,4,5,6,7,8,9];
+var array_num=[1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 var array_multi =[
     {name: "yuliana",age: 19, color:"purple"},
@@ -117,14 +119,14 @@ var array_multi =[
     {name: "wilder",age: 32, color:"yellow"},
     {name: "paula",age: 18, color:"red"},
 
-]
+];
 
 var json_ejm = {
     name: "yuliana",
     last_name: "velasquez",
     phone: "3209428064",
     email:"yyvelasquezg@ufpso.edu.co",
-}
+};
 
 console.log(array_semana);
 console.log(array_gral);
@@ -138,7 +140,7 @@ function action(){
     //console.log(array_num);
     //console.log(array_num.length);
     var result =0;
-    for(let i=0;i<array_num.length;i++){
+    for(let i=0;i <array_num.length;i++){
         result = result+array_num[i]
     }
     document.getElementById("result").innerHTML = "<strong>EL resultado es:"+result+"</strong"
@@ -161,62 +163,70 @@ function action(){
 
 }
 
-var array_numerico =[1,2,3,4,5,6,7,8,9];
-
+var array_numerico=[1, 2, 3, 4,  5, 6, 7, 8, 9];
 function eliminar(){
-    let array_delete = array_numerico.pop();
+  let  array_delete = array_numerico.pop();
+  console.log(array_delete);
+  console.log(array_numerico);
+  document.getElementById("impresion").value=array_numerico;
+
+}
+
+function eliminar_prim(){
+    let array_delete = array_numerico.shift();
     console.log(array_delete);
     console.log(array_numerico);
-    document.getElementById("impresion").value = array_numerico;
-                                                                             
-}
+    document.getElementById("impresion").value= array_numerico;
+  }
 
-function agregar(){
+  function agregar(){
     let num = document.getElementById("num").value;
-    if(isNaN(num) == true){
-        Swal.fire("Solo se aceptan números");
+    let  array_add= array_numerico.push(num);
+    console.log(array_add);
+    console.log(array_numerico);
+    document.getElementById("impresion").value=array_numerico;
+    if(isNaN(num)==true){
+      swal.fire("solo se aceptan numeros ")
+
     }
-    else{
-        let array_add = array_numerico.push(num);
-        console.log(array_add);
-        console.log(array_numerico);
-        document.getElementById("impresion").value = array_numerico;
-    }
-}
+  }
+  function agregar_prim(){
+    let num = document.getElementById("num").value;
+    let  array_add= array_numerico.unshift(num);
+    console.log(array_add);
+    console.log(array_numerico);
+    document.getElementById("impresion").value=array_numerico;
+  }
+  function reves(){
+    document.getElementById("impresion").value= array_numerico.reverse();
+  }
 
-function reves(){
-    document.getElementById("impresion").value = array_numerico.reverse();
+  function limpiar(){
+    document.getElementById("num").value="";
+    document.getElementById("name").value="";
+    document.getElementById("last_name").value="";
+    document.getElementById("result").innerText="";
+  }
+  var nombre_form="Merly Tarazona Sepulveda";
 
-}
+  function search(){
+  let nombre_buscar = document.getElementById("name_search").value;
+ // Swal.fire(nombre_buscar.toLowerCase());
+  //Swal.fire({
+    //text: nombre_buscar.toLowerCase(), //toUpperCase() mayus
+    //title: "Alguna de las cajas se encuentra vacia ",
+    //icon: "error",
+  //})
+ // let word=nombre_buscar.indexOf("e");
+// let word=nombre_buscar.subString(5,15);
 
-function limpiar(){
-    document.getElementById("num").value = "";
-    document.getElementById("name").value = "";
-    document.getElementById("last_name").value = "";
-    document.getElementById("result").innerText = "";
-}
+let word=nombre_buscar.split("");
+  swal.fire(word+"");
+  console.log(word);
 
-var nombre_form = "Yiceth Yuliana Velasquez Gomez";
-
-
-function search(){
-    let nombre_buscar = document.getElementById("name_search").value;
-    //Swal.fire(nombre_buscar.toLowerCase()); // toUpperCase() Mayus
-    // Swal.fire({
-    //     title: nombre_buscar.toLowerCase(),
-    //     text: "Alguna de las cajas de texto se encuentra vacía",
-    //     icon: "error"
-    // });
-    // Swal.fire(nombre_buscar.charAt(0));
-    // let word = nombre_buscar.indexOf('e');
-    // let word = nombre_buscar.lastIndexOf('e');
-    // let word = nombre_buscar.substring(5,15);
-    let word = nombre_buscar.split("");
-    Swal.fire(word+"");
-    console.log(word);
-    let word_com = word.join("");
-    console.log(word_com);
-}
+  let word_com = word.join("");
+  console.log(word_com)
 
 
 
+  }
